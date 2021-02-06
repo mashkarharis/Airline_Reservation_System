a2eb8d3381baf57a6d0b6185319214755269c220
mashkarharis <mashkarharis@gmail.com>
2021-02-06 11:43:29 +0530

    Update DB.sql

--- airline_management/database_manager/DB.sql
+++ airline_management/database_manager/DB.sql
@@ -7,9 +7,11 @@ CREATE TABLE `Admin` (
   `name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(20),
   `password` VARCHAR(45) NOT NULL,
-  `NIC` VARCHAR(45) NOT NULL,
+  `NIC` VARCHAR(45) UNIQUE NOT NULL,
   `age` INT NOT NULL,
   `phone_number` INT,
+  `picture` blob default null,
+  check (age>=18),
   PRIMARY KEY (`Ad_id`));
 
 CREATE TABLE `Membership` (
@@ -25,10 +27,13 @@ CREATE TABLE `User` (
   `password` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `age` INT NOT NULL,
-  `NIC` VARCHAR(45) NOT NULL,
+  `NIC` VARCHAR(45) UNIQUE NOT NULL,
   `email` VARCHAR(20),
   `phone_number` INT,
+  `picture` blob default null,
   `status` VARCHAR(20) default 'PENDING',
+  check (age>=0),
+  check (`status` in ('PENDING','ACTIVE','DELETED')),
   PRIMARY KEY (`u_id`),
   INDEX `member_type_idx` (`member_type` ASC) VISIBLE,
   CONSTRAINT `member_type`
@@ -41,11 +46,12 @@ CREATE TABLE Plane(
 	p_id int auto_increment,
 	capacity int NOT NULL,
 	ptype VARCHAR(20) NOT NULL,
-	pname VARCHAR(30) NOT NULL,
-	engine_no INT NOT NULL,
+	pname VARCHAR(30) UNIQUE NOT NULL,
+	engine_no INT UNIQUE NOT NULL,
 	last_repaired DATETIME NOT NULL,
 	color VARCHAR(20) NOT NULL,
 	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    check (capacity>=0),
 	PRIMARY KEY (P_id)
 );   
 
@@ -54,8 +60,9 @@ CREATE TABLE Airport(
 	park_capacity INT NOT NULL,
 	latitude numeric(10,5) NOT NULL,
 	longitude numeric(10,5)NOT NULL,
-	code VARCHAR(20) NOT NULL,
+	code VARCHAR(20) UNIQUE NOT NULL,
 	description VARCHAR(50) DEFAULT NULL,
+    check (park_capacity>=0),
 	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (a_id)
 );
@@ -66,6 +73,7 @@ CREATE TABLE Route(
 	arrive_a_id INT NOT NULL,
 	description VARCHAR(50) DEFAULT NULL,
 	length numeric(8,2) NOT NULL,
+    check (length>0),
 	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (r_id),
 	FOREIGN KEY (dept_a_id) REFERENCES Airport (a_id) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -99,7 +107,7 @@ CREATE TABLE Flight_Route(
 CREATE TABLE Class(
 	class_id SMALLINT auto_increment,
 	description VARCHAR(50) DEFAULT NULL,
-	type VARCHAR(20) NOT NULL,
+	type VARCHAR(20) UNIQUE NOT NULL,
 	PRIMARY KEY (class_id)
 );
 
@@ -107,6 +115,7 @@ CREATE TABLE Price(
 	f_id INT NOT NULL,
 	class_id SMALLINT NOT NULL,
 	amount DECIMAL(10,2) NOT NULL,
+    check (amount>=0),
 	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	FOREIGN KEY (class_id) REFERENCES Class (class_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (f_id) REFERENCES Flight (f_id) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -122,12 +131,7 @@ CREATE TABLE Seat(
 	PRIMARY KEY (seat_id),
 	FOREIGN KEY (class_id) REFERENCES Class (class_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (p_id) REFERENCES Plane (p_id) ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
-
-
-    
-    
+); 
 
 CREATE TABLE Book(
 	book_id INT auto_increment,
@@ -141,6 +145,13 @@ CREATE TABLE Book(
 	FOREIGN KEY (f_id) REFERENCES Flight(f_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (class_id) REFERENCES Class(class_id) ON DELETE RESTRICT ON UPDATE CASCADE	
 );
+
+--  ============================== INDEX ==============================
+create index ADMIN_SELECT on Admin(NIC);
+create index USER_SELECT on User(NIC);
+create index PLANE_SELECT on Plane(pname);
+create index AIRPORT_SELECT on Airport(a_id);
+
 
 show tables;
 desc admin;
@@ -156,3 +167,5 @@ desc route;
 desc schedule;
 desc seat;
 desc user;
+
+--  ============================== DUMMY DATA FOR START ==============================
