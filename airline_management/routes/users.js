4d17c4a75a6be4834cb9c34f0353bef1d7e25fdf
mashkarharis <mashkarharis@gmail.com>
2021-02-06 21:15:38 +0530

    Update users.js

--- airline_management/routes/users.js
+++ airline_management/routes/users.js
@@ -7,16 +7,16 @@ var router = express.Router();
 router.get('/getallnic', function (req, res, next) {
   const isAuth = true;
   try {
-    if (isAuth) {
+    if (isAuth) {// Redirect
       console.log("Later We will add redirect for session check");
     }
     getAllNIC().then((data) => {
-      res.send(data);
+      res.send(data); //200
     }).catch((err) => {
-      res.send(err);
+      res.send(err); //500
     })
   } catch (err) {
-    res.send(err)
+    res.send(err)//500
   }
 });
 module.exports = router;
