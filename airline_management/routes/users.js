2c9fd5f795a3bf15a1063341ab1f99568edd80c1
mashkarharis <mashkarharis@gmail.com>
2021-02-06 21:13:37 +0530

    Database Connection Added

--- airline_management/routes/users.js
+++ airline_management/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
+
+const { getAllNIC } = require('../models/UserModel');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getallnic', function (req, res, next) {
+  const isAuth = true;
+  try {
+    if (isAuth) {
+      console.log("Later We will add redirect for session check");
+    }
+    getAllNIC().then((data) => {
+      res.send(data);
+    }).catch((err) => {
+      res.send(err);
+    })
+  } catch (err) {
+    res.send(err)
+  }
 });
-
 module.exports = router;
