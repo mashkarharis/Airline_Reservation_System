c2d5d89a770c4905206dbb435d79eecdd2347e7f
mashkarharis <mashkarharis@gmail.com>
2021-02-07 21:55:54 +0530

    Update DB.sql

--- airline_management/database_manager/DB.sql
+++ airline_management/database_manager/DB.sql
@@ -5,7 +5,7 @@ use A12_AirLine_Management;
 CREATE TABLE `Admin` (
   `Ad_id` INT auto_increment,
   `name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(20),
+  `email` VARCHAR(120),
   `password` VARCHAR(45) NOT NULL,
   `NIC` VARCHAR(45) UNIQUE NOT NULL,
   `age` INT NOT NULL,
@@ -28,8 +28,8 @@ CREATE TABLE `User` (
   `name` VARCHAR(45) NOT NULL,
   `age` INT NOT NULL,
   `NIC` VARCHAR(45) UNIQUE NOT NULL,
-  `email` VARCHAR(20),
-  `phone_number` INT,
+  `email` VARCHAR(120),
+  `phone_enumber` bigint,
   `picture` blob default null,
   `status` VARCHAR(20) default 'PENDING',
   check (age>=0),
@@ -50,7 +50,6 @@ CREATE TABLE Plane(
 	engine_no INT UNIQUE NOT NULL,
 	last_repaired DATETIME NOT NULL,
 	color VARCHAR(20) NOT NULL,
-	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     check (capacity>=0),
 	PRIMARY KEY (P_id)
 );   
@@ -63,8 +62,7 @@ CREATE TABLE Airport(
 	code VARCHAR(20) UNIQUE NOT NULL,
 	description VARCHAR(50) DEFAULT NULL,
     check (park_capacity>=0),
-	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (a_id)
+    PRIMARY KEY (a_id)
 );
   
 CREATE TABLE Route(
@@ -72,10 +70,9 @@ CREATE TABLE Route(
 	dept_a_id INT NOT NULL,
 	arrive_a_id INT NOT NULL,
 	description VARCHAR(50) DEFAULT NULL,
-	length numeric(8,2) NOT NULL,
+	length numeric(38,2) NOT NULL,
     check (length>0),
-	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (r_id),
+    PRIMARY KEY (r_id),
 	FOREIGN KEY (dept_a_id) REFERENCES Airport (a_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (arrive_a_id) REFERENCES Airport (a_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -84,15 +81,13 @@ CREATE TABLE Schedule(
 	description VARCHAR(50) DEFAULT NULL,
 	arrival_time DATETIME NOT NULL,
 	departure_time DATETIME NOT NULL,
-	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (s_id)
+    PRIMARY KEY (s_id)
 );
 CREATE TABLE Flight(
 	f_id INT auto_increment,
 	p_id INT NOT NULL,
 	s_id INT NOT NULL,
-	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (f_id),
+    PRIMARY KEY (f_id),
 	FOREIGN KEY (p_id) REFERENCES Plane(p_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (s_id) REFERENCES Schedule(s_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -116,8 +111,7 @@ CREATE TABLE Price(
 	class_id SMALLINT NOT NULL,
 	amount DECIMAL(10,2) NOT NULL,
     check (amount>=0),
-	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	FOREIGN KEY (class_id) REFERENCES Class (class_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES Class (class_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (f_id) REFERENCES Flight (f_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
@@ -127,8 +121,7 @@ CREATE TABLE Seat(
 	class_id SMALLINT NOT NULL,
 	p_id INT NOT NULL,
 	seat_no INT NOT NULL,
-	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (seat_id),
+    PRIMARY KEY (seat_id),
 	FOREIGN KEY (class_id) REFERENCES Class (class_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (p_id) REFERENCES Plane (p_id) ON DELETE RESTRICT ON UPDATE CASCADE
 ); 
@@ -139,8 +132,7 @@ CREATE TABLE Book(
 	f_id INT NOT NULL,
 	class_id SMALLINT NOT NULL,
 	time_date DATETIME NOT NULL,
-	last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (book_id),
+    PRIMARY KEY (book_id),
 	FOREIGN KEY (u_id) REFERENCES User(u_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (f_id) REFERENCES Flight(f_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (class_id) REFERENCES Class(class_id) ON DELETE RESTRICT ON UPDATE CASCADE	
@@ -168,4 +160,64 @@ desc schedule;
 desc seat;
 desc user;
 
---  ============================== DUMMY DATA FOR START ==============================
+--  ============================== DUMMY DATA FOR START	 ==============================
+-- admin:
+INSERT INTO admin VALUES ("01","Rashmi","rashmi.18@cse.mrt.ac.lk","rashmi","995030225V","22","0702036662",null);
+INSERT INTO admin VALUES ("02","Sandali","sandali.18@cse.mrt.ac.lk","sandali","985030225V","23","0702036663",null);
+
+-- plane:
+INSERT INTO plane VALUES("01","180","A320","A320-200","2","2020-11-30","White");
+INSERT INTO plane VALUES("02","145","A319","A319-200","3","2020-02-22","White");
+-- membership:
+INSERT INTO membership VALUES("Guest","Guest customer","0");
+INSERT INTO membership VALUES("Reg_frequent","Registered and frequent customer","5");
+INSERT INTO membership VALUES("Reg_gold","Registered and gold customer","9");
+
+-- user:
+INSERT INTO user VALUES("01","Guest","Sri Lanka","111111","Kamal","23","983646234V","kamal@gmail.com","0703746253",null,null);
+INSERT INTO user VALUES("02","Reg_frequent","Sri Lanka","111111","Nishani","23","983633234V","nishani@gmail.com","0703236253",null,null);
+INSERT INTO user VALUES("03","Reg_gold","Canada","111111","James","23","223456","james@gmail.com","0771234567",null,null);
+INSERT INTO user VALUES("04","Reg_gold","Korea","123456","Kim","30","2234456","kim@gmail.com","022716345",null,null);
+INSERT INTO user VALUES("05","Guest","Sri Lanka","123456","Supun","50","712535663V","supun@gmail.com","0701234565",null,null);
+INSERT INTO user VALUES("06","Guest","Australia","123446","Sam","66","735204856","sam@gmail.com","07926365448726",null,null);
+INSERT INTO user VALUES("07","Guest","Australia","121212","Jenny","36","732304856","jenny@gmail.com","0792624235448726",null,null);
+INSERT INTO user VALUES("08","Reg_frequent","Sri Lanka","121212","Akashi","22","992637128V","akashi@gmail.com","0771234567",null,null);
+INSERT INTO user VALUES("09","Reg_frequent","Sri Lanka","555555","Amani","23","982637128V","amani@gmail.com","0771234577",null,null);
+INSERT INTO user VALUES("10","Guest","India","666666","Amir","23","82347863","amir@gmail.com","34545644577",null,null);
+
+-- schedule:
+INSERT INTO schedule VALUES("01",NULL,"2020-10-09 05:30:00","2020-10-09 08:30:00");
+INSERT INTO schedule VALUES("03",NULL,"2020-10-09 05:30:00","2020-10-09 08:30:00");
+
+-- class:
+INSERT INTO class VALUES("01",null,"1st class");
+INSERT INTO class VALUES("02",null,"2nd class");
+
+-- flight:
+INSERT INTO flight VALUES("01","01","01");
+INSERT INTO flight VALUES("02","02","01");
+
+-- book:
+INSERT INTO book VALUES("01","03","01","01","2020-10-09 05:30:00");
+INSERT INTO book VALUES("02","06","02","02","2020-12-23 05:30:00");
+
+-- seat:
+INSERT INTO seat VALUES("01","01","02","34");
+INSERT INTO seat VALUES("02","02","01","155");
+
+-- price:
+INSERT INTO price VALUES("01","01","30000");
+INSERT INTO price VALUES("02","01","60000");
+
+-- airport:
+INSERT INTO airport VALUES("01","10000","500","10000","100",null);
+INSERT INTO airport VALUES("02","30000","200","10000","57",null);
+
+-- route:
+INSERT INTO route VALUES("01","02","01",NULL,"100000");
+INSERT INTO route VALUES("02","01","02",NULL,"3000000");
+
+-- flight_route:
+INSERT INTO flight_route VALUES("01","02");
+INSERT INTO flight_route VALUES("01","01");
+
