6688192e69c71a5fb2ee90c2d3d4443dbfeab4ab
Rashmi Prabodha <61895411+rashmi9913@users.noreply.github.com>
2021-02-14 12:23:00 +0530

    Admin

--- airline_management/models/AdminModel.js
+++ airline_management/models/AdminModel.js
@@ -1,18 +1,17 @@
 const { pool } = require('../database_manager/mysqlpool');
-var UserModel = {
-    addUser: addUser,
-    updateUser:updateUser,
-    deleteUser:deleteUser,
-    getAllNIC: getAllNIC
+var AdminModel = {
+    addAdmin: addAdmin,
+    updateAdmin:updateAdmin,
+    deleteAdmin:deleteAdmin
 }
 
-function addUser(array) {
+function addAdmin(array) {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, conn) => {
             if (err || conn.state === "disconnected") {
                 reject("Database Error");
             }
-            conn.query("INSERT INTO user VALUES(?,?,?,?,?,?,?,?,?,?);",array, function (error, rows, fields) {
+            conn.query("INSERT INTO admin VALUES(?,?,?,?,?,?,?,?);",array, function (error, rows, fields) {
                 conn.release();
                 if (error) {
                     reject(error);
@@ -24,13 +23,13 @@ function addUser(array) {
     });
 }
 
-function updateUser(array) {
+function updateAdmin(array) {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, conn) => {
             if (err || conn.state === "disconnected") {
                 reject("Database Error");
             }
-            conn.query("UPDATE user set member_type=?,country=?,password=?,name=?,age=?,NIC=?,email=?,phone_enumber=?,picture=?,status=? where u_id=?",array, function (error, rows, fields) {
+            conn.query("UPDATE admin set name=?,email=?,password=?,NIC=?,country=?,age=?,phone_enumber=?,picture=? where Ad_id=?",array, function (error, rows, fields) {
                 conn.release();
                 if (error) {
                     reject(error);
@@ -42,13 +41,13 @@ function updateUser(array) {
     });
 }
 
-function deleteUser(array) {
+function deleteAdmin(array) {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, conn) => {
             if (err || conn.state === "disconnected") {
                 reject("Database Error");
             }
-            conn.query("delete from user where u_id=?",array, function (error, rows, fields) {
+            conn.query("delete from admin where Ad_id=?",array, function (error, rows, fields) {
                 conn.release();
                 if (error) {
                     reject(error);
@@ -60,24 +59,4 @@ function deleteUser(array) {
     });
 }
 
-function getAllNIC() {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err || conn.state === "disconnected") {
-                reject("Database Error");
-            }
-            conn.query("select NIC from User;", function (error, rows, fields) {
-                conn.release();
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(rows[0]);
-                }
-            });            
-        })
-    });
-}
-
-
-
-module.exports=UserModel;
+module.exports=AdminModel;
--- airline_management/models/Test.js
+++ airline_management/models/Test.js
@@ -1,10 +1,24 @@
 const { addMembershipType, updateMembershipType, deleteMembershipType } = require("./MembershipModel");
+const { addUserType, updateMembership, deleteUser } = require("./UserModel");
+const { addAdmin, updateAdmin, deleteAdmin } = require("./AdminModel");
 
 //addMembershipType(["Normal_3","Just for fun",10]).then((res)=>{console.log("pass")}).catch((res)=>{console.log("failed")});
 
 //updateMembershipType(["Test purpose",12,"Normal"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
 
 deleteMembershipType(["Normal_1"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
+
+//addUser([Guest","Sri Lanka","111111","Kamal","23","983646234V","kamal@gmail.com","0703746253",null,null]).then((res)=>{console.log("pass")}).catch((res)=>{console.log("failed")});
+
+//updateUser([Guest","Canada","111111","Kamal","23","983646234V","kamal@gmail.com","0703746253",null,null,"01"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
+
+deleteUser(["01"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
+
+//addAdmin(["Rashmi","rashmi.18@cse.mrt.ac.lk","rashmi","995030225V","Sri Lanka","22","0702036662",null]).then((res)=>{console.log("pass")}).catch((res)=>{console.log("failed")});
+
+//updateAdmin(["Rashmi","rashmi.18@cse.mrt.ac.lk","rashmi","995030225V","Australia","23","0702036662",null,"01"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
+
+deleteAdmin(["01"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
 
 
 
