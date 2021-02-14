372ef0e2fd8840e335bd55348d2885a04e8f4eca
hashani98 <77402471+hashani98@users.noreply.github.com>
2021-02-14 11:07:21 +0530

    Update Test.js

--- airline_management/models/Test.js
+++ airline_management/models/Test.js
@@ -7,6 +7,22 @@ const { addMembershipType, updateMembershipType, deleteMembershipType } = requir
 deleteMembershipType(["Normal_1"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
 
 
+const { addBookType, updateBookType, deleteBookType } = require("./BookModel");
 
+addBookType(["06","03","01","Economic","2020-10-09 05:30:00"]).then((res)=>{console.log("pass")}).catch((res)=>{console.log("failed")});
+
+updateBookType(["03","01","Economic","2020-10-09 05:30:00","01"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
+
+deleteBookType(["02"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
+
+
+
+const { addClassType, updateClassType, deleteClassType } = require("./ClassModel");
+
+addClassType(["Economic","Normal Travels"]).then((res)=>{console.log("pass")}).catch((res)=>{console.log("failed")});
+
+updateClassType(["Faster Travels","Economic"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
+
+deleteClassType(["Economic"]).then((res)=>{console.log(res);console.log("pass")}).catch((res)=>{console.log(res);console.log("failed")});
 
 
