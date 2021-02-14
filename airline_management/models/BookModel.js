3c438183f92adce0c8f65e2405765f4b61e59e79
hashani98 <77402471+hashani98@users.noreply.github.com>
2021-02-14 11:00:39 +0530

    Create BookModel.js

---
+++ airline_management/models/BookModel.js
@@ -0,0 +1,62 @@
+const { pool } = require('../database_manager/mysqlpool');
+var BookModel = {
+    addBookType: addBookType,
+    updateBookType:updateBookType,
+    deleteBookType:deleteBookType
+}
+
+function addBookType(array) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if (err || conn.state === "disconnected") {
+                reject("Database Error");
+            }
+            conn.query("INSERT INTO book VALUES(?,?,?,?,?);",array, function (error, rows, fields) {
+                conn.release();
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(rows[0]);
+                }
+            });            
+        })
+    });
+}
+
+function updateBookType(array) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if (err || conn.state === "disconnected") {
+                reject("Database Error");
+            }
+            conn.query("UPDATE book set u_id=?,f_id=?,type=?,time_date=? where book_id=?",array, function (error, rows, fields) {
+                conn.release();
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(rows[0]);
+                }
+            });            
+        })
+    });
+}
+
+function deleteBookType(array) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if (err || conn.state === "disconnected") {
+                reject("Database Error");
+            }
+            conn.query("delete from book where book_id=?",array, function (error, rows, fields) {
+                conn.release();
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(rows[0]);
+                }
+            });            
+        })
+    });
+}
+
+module.exports=BookModel;
