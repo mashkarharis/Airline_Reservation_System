b72b3df3e926c27823d909a9e5e68989a6f59705
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-02-21 12:04:39 +0530

    Added layout and other UI s

--- airline_management/routes/index.js
+++ airline_management/routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("landingPage/index.ejs", {
+    title: "Express",
+    layout: false,
+  });
 });
+router.get("/login", function (req, res, next) {
+  res.render("login", {
+    title: "Express",
+    layout: false,
+  });
+});
+router.get("/register", function (req, res, next) {
+  res.render("register", {
+    title: "Express",
+    layout: false,
+  });
+});
+// router.get("/", function (req, res, next) {
+//   res.render("landingPage/index.ejs", {
+//     title: "Express",
+//     layout: "layouts/layoutSecond.ejs",
+//   });
+// });
 
 module.exports = router;
--- airline_management/routes/users.js
+++ airline_management/routes/users.js
@@ -1,22 +1,28 @@
-var express = require('express');
+var express = require("express");
 
-const { getAllNIC } = require('../models/UserModel');
+const { getAllNIC } = require("../models/UserModel");
 var router = express.Router();
 
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Users" });
+});
 /* GET users listing. */
-router.get('/getallnic', function (req, res, next) {
+router.get("/getallnic", function (req, res, next) {
   const isAuth = true;
   try {
-    if (isAuth) {// Redirect
+    if (isAuth) {
+      // Redirect
       console.log("Later We will add redirect for session check");
     }
-    getAllNIC().then((data) => {
-      res.send(data); //200
-    }).catch((err) => {
-      res.send(err); //500
-    })
+    getAllNIC()
+      .then((data) => {
+        res.send(data); //200
+      })
+      .catch((err) => {
+        res.send(err); //500
+      });
   } catch (err) {
-    res.send(err)//500
+    res.send(err); //500
   }
 });
 module.exports = router;
