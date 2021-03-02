e56aaefaafd52e9c24d7bf76dd295d6cca059671
hashani98 <77402471+hashani98@users.noreply.github.com>
2021-03-02 19:06:31 +0530

    Update BookModel.js

--- airline_management/models/BookModel.js
+++ airline_management/models/BookModel.js
@@ -1,11 +1,49 @@
 const { pool } = require('../database_manager/mysqlpool');
 var BookModel = {
+    addBookType: addBookType,
+    updateBookType:updateBookType,
     getBookdata:getBookdata,
     remove_booking:remove_booking,
     doguestbooking:doguestbooking,
     getguestData:getguestData,
     domembooking:domembooking
 }
+function addBookType(array) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if (err || conn.state === "disconnected") {
+                reject("Database Error");
+            }
+            conn.query("INSERT INTO book VALUES(?,?,?,?,?);",array, function (error, rows, fields) {
+                conn.release();
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(rows[0]);
+                }
+            });            
+        })
+    });
+}
+
+function updateBookType(array) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if (err || conn.state === "disconnected") {
+                reject("Database Error");
+            }
+            conn.query("UPDATE book set u_id=?,f_id=?,type=?,time_date=? where book_id=?",array, function (error, rows, fields) {
+                conn.release();
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(rows[0]);
+                }
+            });            
+        })
+    });
+}
+
 function doguestbooking(body) {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, conn) => {
