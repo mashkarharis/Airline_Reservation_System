ece37a02a7f9a146ac87e4a46a1db688923be8e2
hashani98 <77402471+hashani98@users.noreply.github.com>
2021-03-02 19:01:05 +0530

    Update BookModel.js

--- airline_management/models/BookModel.js
+++ airline_management/models/BookModel.js
@@ -2,7 +2,8 @@ const { pool } = require('../database_manager/mysqlpool');
 var BookModel = {
     addBookType: addBookType,
     updateBookType:updateBookType,
-    deleteBookType:deleteBookType
+    getBookdata:getBookdata,
+    remove_booking:remove_booking
 }
 
 function addBookType(array) {
@@ -41,22 +42,41 @@ function updateBookType(array) {
     });
 }
 
-function deleteBookType(array) {
+function remove_booking(booking_id){
+
     return new Promise((resolve, reject) => {
         pool.getConnection((err, conn) => {
             if (err || conn.state === "disconnected") {
                 reject("Database Error");
-            }
-            conn.query("delete from book where book_id=?",array, function (error, rows, fields) {
+            } 
+            conn.query("call removebooking(?)",[booking_id], function (error, rows, fields) {
                 conn.release();
                 if (error) {
                     reject(error);
                 } else {
-                    resolve(rows[0]);
+                    resolve(rows);
                 }
             });            
         })
     });
 }
-
+function getBookdata(email){
+    console.log("ABC");
+    console.log(email);
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if (err || conn.state === "disconnected") {
+                reject("Database Error");
+            }
+            conn.query("select * from basic_book_data where email=?;",[email], function (error, rows, fields) {
+                conn.release();
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(rows);
+                }
+            });            
+        })
+    });
+}
 module.exports=BookModel;
